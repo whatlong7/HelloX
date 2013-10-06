837eda093972c81782f7ad625772f75314616ad3
whatlong7 <whatlong7@gmail.com>
2013-10-06 12:35:03 +0800

    你好

---
+++ HelloX/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  HelloX
+//
+//  Created by 龙 秦 on 13-10-6.
+//  Copyright (c) 2013年 龙 秦. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
